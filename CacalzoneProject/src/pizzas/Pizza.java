9df1459803767bd2f28988bb7987d08f33193219
e22500013 <anis.lafrad@etudiant.univ-brest.fr>
2025-11-05 15:30:05 +0100

    Phase 1.0 : Initialisation de la classe Pizza.java

--- CacalzoneProject/src/pizzas/Pizza.java
+++ CacalzoneProject/src/pizzas/Pizza.java
@@ -1,6 +1,35 @@
 package pizzas;
 
 // A compléter
+/**
+ * Classe Pizza définit par un nom et un type de pizza
+ *
+ * @author Anis LAFRAD
+ *
+ */
 public class Pizza {
+	//--------------- ATTRIBUTS ---------------
+	String nom;
+	TypePizza untype;
+	//--------------- CONSTRUCTEUR ---------------
+	public Pizza(String nom, TypePizza untype) {
+		super();
+		this.nom = nom;
+		this.untype = untype;
+	}
+	//--------------- GETTERS // SETTERS ---------------
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public TypePizza getUntype() {
+		return untype;
+	}
+	public void setUntype(TypePizza untype) {
+		this.untype = untype;
+	}
+	
   
 }
